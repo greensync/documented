954c5ee181d217b055b5aea6ff11e2b043328e3f
maedi <maediprichard@gmail.com>
2023-10-27 12:04:18 +1100

    Enable and disable tracer via comment

--- lib/documented.rb
+++ lib/documented.rb
@@ -1,12 +1,26 @@
+require_relative './config'
+require_relative './tracer'
+
 module Documented
   class << self
     def setup()
-      @@config = Config.new()
+      @@config = Config.new
+      @@tracer = Tracer.new(@@config.blocklist)
 
       output_path = File.join(@@config.app_path, @@config.output_directory)
       Dir.mkdir(output_path) unless Dir.exist? output_path
 
       @@renderer = Renderer.new(@@config.gem_path, output_path)
+
+      self
+    end
+
+    def config
+      @@config
+    end
+
+    def tracer
+      @@tracer
     end
 
     # Allow end-user to override config.
